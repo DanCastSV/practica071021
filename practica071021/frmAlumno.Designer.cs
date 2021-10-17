3fc31ad6dbb9a816c0f3ae994f4a3e0087025cf5
Daniel Castillo <ec1335784@gmail.com>
2021-10-16 19:21:36 -0600

    Arreglo

--- practica071021/frmAlumno.Designer.cs
+++ practica071021/frmAlumno.Designer.cs
@@ -67,9 +67,9 @@ namespace practica071021
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(132, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Ingreso de alumnos";
             // 
             // label2
             // 
